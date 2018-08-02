5af42f98ca84a3549f1748becd1613dbb3d4cfce
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-08-02 09:20:18 -0400

    clean up and create input component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import Input from './components/input/Input'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Input name="test" label="Test" />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/components/Input/Input.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Input = (label, name) => {
+  return (
+    <div>
+      <label for={this.props.name}> {this.props.label} </label>
+      <input type="text" name={this.props.name} />
+    </div>
+  )
+}
+
+export default Input
