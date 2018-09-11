6b1782e2cd425e919dbcf2788bc450f979253223
Jeffrey Heller <heller.jeffrey@gmail.com>
2018-09-11 09:40:22 -0400

    add enzyme + tests for error component

---
+++ src/components/Error/Error.test.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import Error from './Error'
+
+describe('Error', () => {
+  let props
+  let mountedError
+  const error = () => {
+    if (!mountedError) {
+      mountedError = mount(<Error {...props} />)
+    }
+    return mountedError
+  }
+
+  beforeEach(() => {
+    props = {
+      error: undefined
+    }
+    mountedError = undefined
+  })
+
+  //   Tests Here:
+  it('always renders a div', () => {
+    const divs = error().find('div')
+    expect(divs.length).toBeGreaterThan(0)
+  })
+})
