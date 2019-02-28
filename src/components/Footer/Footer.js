62c53f6c79fb5adb4a2072dd104e27b70f830096
Jeffrey Heller <heller.jeffrey@gmail.com>
2019-02-28 10:24:58 -0500

    footer css module

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,26 +1,21 @@
 import React from 'react'
+import styles from './Footer.module.css'
 
 const Footer = () => {
   return (
-    <footer className="flex">
+    <footer className={styles.footer}>
       <p>Created by Jeff Heller</p>
-      <ul className="flex footer-icons">
-        <li>
-          <a href="https://github.com/Jheller11">
-            <span className="fa fa-github-square fa-lg" />
-          </a>
-        </li>
-        <li>
-          <a href="mailto:heller.jeffrey@gmail.com">
-            <span className="fa fa-envelope fa-lg" />
-          </a>
-        </li>
-        <li>
-          <a href="https://www.linkedin.com/in/jeffreyaheller/">
-            <span className="fa fa-linkedin fa-lg" />
-          </a>
-        </li>
-      </ul>
+      <nav className={styles.nav}>
+        <a href="https://github.com/Jheller11">
+          <span className="fa fa-github-square fa-lg" />
+        </a>
+        <a href="mailto:heller.jeffrey@gmail.com">
+          <span className="fa fa-envelope fa-lg" />
+        </a>
+        <a href="https://www.linkedin.com/in/jeffreyaheller/">
+          <span className="fa fa-linkedin fa-lg" />
+        </a>
+      </nav>
     </footer>
   )
 }
